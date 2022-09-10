f6c6f0af90ca3f94c7e7b5ae980209e899625a63
Yurii <yurasykal@gmail.com>
2022-09-10 19:19:25 +0100

    version1 (fix)

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,13 +1,17 @@
 package com;
 
 import com.command.Commands;
+import com.config.FlyWayConfig;
+import com.config.HibernateFactoryUtil;
 import com.util.UserInputUtil;
+
+import javax.persistence.EntityManager;
 
 public class Main {
     public static void main(String[] args) {
-//        EntityManager entityManager = HibernateFactoryUtil.getEntityManager();
-//        entityManager.isOpen();
- //       FlyWayConfig.getInstance().migrate();
+        EntityManager entityManager = HibernateFactoryUtil.getEntityManager();
+        entityManager.isOpen();
+        FlyWayConfig.getInstance().migrate();
         final Commands[] values = Commands.values();
         boolean exit;
 
